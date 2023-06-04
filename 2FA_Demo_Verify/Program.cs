083e261c9e07b261d8baddd94520db3021e43f1d
TPM-28 <felicien.latour@hotmail.com>
2023-06-04 11:30:42 +0200

    11:30

---
+++ 2FA_Demo_Verify/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Google.Authenticator;
+
+namespace FA_Demo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var twoFactorAuthenticator = new TwoFactorAuthenticator();
+            var result = VerifyTwoFactorCode(twoFactorAuthenticator);
+
+            Console.WriteLine(result);
+        }
+
+        public static string VerifyTwoFactorCode(TwoFactorAuthenticator authenticator)
+        {
+            Console.WriteLine("Entrez la clé secrète :");
+            var secretKey = Console.ReadLine();
+
+            Console.WriteLine("Entrez le code 2FA généré par l'application :");
+            var userCode = Console.ReadLine();
+
+            bool isCodeValid = authenticator.ValidateTwoFactorPIN(secretKey, userCode);
+
+            return isCodeValid ? "Code 2FA valide !" : "Code 2FA invalide !";
+        }
+    }
+}
